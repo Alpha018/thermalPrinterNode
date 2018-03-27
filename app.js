83853223efd0704c276a2869f8953d039f3a7a58
blacksinx <tas001@alumnos.ucn.cl>
2018-03-27 15:44:23 -0300

    Added: se completa el post para imprimir

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use('/api/gestionlegal', gestionLegal_routes);
 app.use('/api/asunto', asunto_routes);
 app.use('/api/solicitud', solicitud_routes);*/
 
-app.get('/printer', (req, res) =>{
+app.post('/printer', (req, res) =>{
+    const params = req.body;
     const hora = new Date();
     let printer = require('node-thermal-printer');
     printer.init({
@@ -49,8 +50,8 @@ app.get('/printer', (req, res) =>{
     printer.println('TICKET DE ALMUERZO');
     printer.drawLine();
     printer.alignLeft();
-    printer.println('Nombre: Juanito Perez        - RUT: 1-9');
-    printer.println('Tipo: Almuerzo               - Empresa: incacoya');
+    printer.println('Nombre: ' + params.nombre + '        - RUT: ' + params.rut);
+    printer.println('Tipo: ' + params.tipo + '               - Empresa: '+ params.empresa);
     printer.alignCenter();
     printer.println('Dia: ' + hora.getDay() + ' - Hora: ' + hora.getHours() + ':' + hora.getMinutes());
     printer.drawLine();
@@ -59,8 +60,15 @@ app.get('/printer', (req, res) =>{
     printer.execute(function (err) {
         if(err) {
             console.log('print error: ' + err);
+            res.status(500).send({
+                desc: 'Printer Error',
+                err: err.message
+            })
         } else {
             console.log('print DONE!!');
+            res.status(200).send({
+                desc: 'Printer done!'
+            })
         }
     })
 });
